bdd7a12fd0ee135f38de4f0fb1a1175bceeba0ff
JSisques <javierplazasisques@gmail.com>
2024-07-24 11:29:50 +0200

    Add backup.sh

---
+++ scripts/backup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Variables
+SOURCE_DIR="/home/javi/minecraft-server/data"
+BACKUP_DIR="/home/javi/minecraft-server/backups"
+TIMESTAMP=$(date +"%Y-%m-%d")
+BACKUP_FILE="$TIMESTAMP.tar.gz"
+
+# Crear directorio de respaldo si no existe
+mkdir -p $BACKUP_DIR
+
+# Realizar la copia de seguridad
+tar -zcvf $BACKUP_DIR/$BACKUP_FILE -C $SOURCE_DIR .
+
+# Imprimir mensaje de éxito
+echo "Copia de seguridad realizada con éxito: $BACKUP_DIR/$BACKUP_FILE"
